4fee5d61510e2c64838b23f5d65e5e08a06061d5
zain huda <mhuda@uwo.ca>
2018-09-28 23:22:12 -0400

    lc 165

---
+++ leetcode/165.py
@@ -0,0 +1,20 @@
+class Solution:
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        a = [int(x) for x in version1.split(".")]
+        
+        b = [int(x) for x in version2.split(".")]
+        
+        for i in range(max(len(a),len(b))):
+            v1Val = a[i] if i < len(a) else 0
+            v2Val = b[i] if i < len(b) else 0
+            if v1Val > v2Val:
+                return 1
+            elif v2Val > v1Val:
+                return -1
+        return 0
+        
