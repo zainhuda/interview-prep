0336f0e46ee5fe1a70000841c820e14e21570ff0
zain huda <mhuda@uwo.ca>
2018-10-10 23:33:53 -0600

    2 problems

---
+++ dailycodingproblem/day7.java
@@ -0,0 +1,11 @@
+/*
+Facebook
+Given the mapping a = 1, b = 2, .., z = 26 and an encoded message,
+count number of ways it can be decoded.
+For example, the message '111', would give 3,
+it can be decoded as 'aaa', 'ka', and 'ak'
+Messages are decodable
+*/
+public class Solution {
+  
+}
