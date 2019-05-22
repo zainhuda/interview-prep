e027345daf3a202cdc362bb8fc85980c93916eb0
zain huda <mhuda@uwo.ca>
2019-05-21 20:18:15 -0400

    83

--- leetcode/83.java
+++ leetcode/83.java
@@ -1,17 +1,27 @@
-class removeDuplicate{
-  public static ListNode deleteDuplicates(ListNode head) {
-    Set<Integer> store = new HashSet<>();
-    ListNode node = head, prev = null;
-    while(node != null) {
-      if (store.contains(node.data)) {
-        prev.next = node.next;
-        node = node.next;
-        continue;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+      if (head == null || head.next == null) return head;
+      ListNode dummy = new ListNode(0);
+      dummy.next = head;
+      ListNode curr = dummy.next.next;
+      ListNode prev = dummy.next;
+      while (curr != null) {
+          if (curr.val == prev.val) {
+              prev.next = curr.next;
+              curr = prev.next;
+          } else {
+              curr = curr.next;
+              prev = prev.next;
+          }
       }
-      store.add(node.data);
-      prev = node;
-      node = node.next;
-    }
-    return head;
+      return dummy.next;
   }
 }
