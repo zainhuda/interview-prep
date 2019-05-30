2aa2d0ff037368111d5764bc9e94e30af498f4ec
zain huda <mhuda@uwo.ca>
2019-05-29 22:49:00 -0400

    26 revisited

--- leetcode/26.java
+++ leetcode/26.java
@@ -1,14 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // two pointers, current node we are at, and then we keep going i.
-        // remove numbers from array by moving all subsequent numbers left by one
-        // 2nd pointer is the the length that we go to
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[j] != nums[i]) {
-                nums[++j] = nums[i];
+        int slow = 0;
+        for (int i = 0; i<nums.length; i++) {
+            if (nums[slow] != nums[i]) {
+                slow++;
+                nums[slow] = nums[i];
             }
         }
-        return j+1;
+        return slow+1;
     }
 }
