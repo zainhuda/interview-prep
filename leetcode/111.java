a9af061cfa9a6ac9d51b9b9e92b7bec80dba8fda
zain huda <mhuda@uwo.ca>
2019-07-09 23:36:58 -0400

    101, summertime in paris

---
+++ leetcode/111.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } 
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        } 
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
