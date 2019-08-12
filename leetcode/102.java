aec789a77c366bd796de167a1b9ee990bc5973f1
Zain Huda <mhuda@uwo.ca>
2019-08-12 17:34:55 -0400

    102, elevate

---
+++ leetcode/102.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // iterative sol using queue, this is hard
+    // for each node visited we add it to the queue, and for loop for the count of queue (visitng the nodes in the level)
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+}
